6c81282708d47f720c44aa41932152c330a7eb36
Manish <arayanmanish@gmail.com>
2026-06-11 16:23:27 +0530

    navbar

--- src/components/BrandSection.jsx
+++ src/components/BrandSection.jsx
@@ -4,7 +4,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
 
-const BrandSection = ({ title, images }) => {
+const BrandSection = ({ title, images, media }) => {
 
   const [popupIndex, setPopupIndex] = useState(null);
 
